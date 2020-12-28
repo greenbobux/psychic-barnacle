2a55be44434911de180ac022829ac3d46c2641bc
Bobux scripter 2946 <70306120+hjaoehyfae@users.noreply.github.com>
2020-12-28 12:52:30 -0800

    Ui

--- src/ReplicatedStorage/UI.FX/modules/UITypes.lua
+++ src/ReplicatedStorage/UI.FX/modules/UITypes.lua
@@ -3,13 +3,13 @@ return setmetatable({
 },{
     __index = function(tbl,index)
         local Type = nil
-        if type == index then
-        
+        if tbl.type == index then
+            Type = tbl.Type
         end
         for _,v in pairs(tbl) do
             for _,k in pairs(v.alias) do
                 if index == k then
-                    
+                    Type = tbl.Type
                 end
             end
         end
